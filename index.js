4456df556b0d5e93742ba9ef469750f4781461ca
Brett <brett@cto.ai>
2021-09-10 19:09:13 -0300

    push

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const { sdk } = require('@cto.ai/sdk')
-const http = require('http')
-
+const { ux, sdk } = require('@cto.ai/sdk')
 async function main() {
-  const requestListener = function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.write('Hello World!')
-    res.end()
-  }
-
-  const server = http.createServer(requestListener)
-  server.listen(8080, function () {
-    sdk.log('server started!')
-  })
+  
+console.log(process.env)
 }
 main()
